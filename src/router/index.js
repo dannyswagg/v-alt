ab308cdc40521f3c59ae90520af24559b2e55311
Ugiomoh Daniel <ugiomohd@yahoo.com>
2023-03-18 03:25:44 +0100

    done

--- src/router/index.js
+++ src/router/index.js
@@ -34,19 +34,6 @@ const router = createRouter({
       name: "404",
       component: () =>import("../views/ErrorView.vue")
     },
-    {path: "/404",
-    name: "page-not-found",
-    component: () => import("../views/ErrorView.vue")
-    },
-    {
-      path: "/:catchAll(.*)",
-      redirect: "/404"
-    },
-    {
-      path: "/:catchAll(.*)",
-      name: "page-not-found",
-      component: () => import("../views/ErrorView.vue")
-    },
   ],
 })
 
